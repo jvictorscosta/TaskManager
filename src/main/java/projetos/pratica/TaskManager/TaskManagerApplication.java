13b5bf9c00e587fe567f96ab76cd77656210dc1b
jvictorscosta <jvictorscosta@gmail.com>
2025-03-12 18:57:19 -0300

    t1

--- src/main/java/projetos/pratica/TaskManager/TaskManagerApplication.java
+++ src/main/java/projetos/pratica/TaskManager/TaskManagerApplication.java
@@ -2,6 +2,8 @@ package projetos.pratica.TaskManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class TaskManagerApplication {
@@ -9,5 +11,6 @@ public class TaskManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskManagerApplication.class, args);
 	}
+	
 
 }
